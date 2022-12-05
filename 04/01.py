3d809a86f2abba7d610c6adf15b208c747f8323b
Matúš Pikuliak <matus.pikuliak@gmail.com>
2022-12-05 09:52:01 +0100

    Day 04

--- 04/01.py
+++ 04/01.py
@@ -2,6 +2,7 @@ nums = [
     map(int, line.strip().replace(',', '-').split('-'))
     for line in open('input')
 ]
+
 ranges = [
     (set(range(a, b + 1)), set(range(c, d + 1)))
     for a, b, c, d in nums
